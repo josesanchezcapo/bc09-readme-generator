538a8c6d9fcf655f24771afefb6209a44bcf5dfe
Jose Sanchez-Capo <josesanchezcapo@me.com>
2020-12-05 08:28:20 -0600

    fixing not loading module

---
+++ my_modules/readmeGenerator.js
@@ -0,0 +1,15 @@
+function readmeGenerator(myResponses) {
+
+    const readmeFormat = `
+    
+    # ${myResponses.projectTitle}
+    
+    `
+
+    // return readme format
+    return readmeFormat;
+    
+
+}
+
+module.exports = readmeGenerator;
